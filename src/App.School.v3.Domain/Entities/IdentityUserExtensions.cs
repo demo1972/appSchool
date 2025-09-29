7f2bcd1eeeb6879b100308d297e8f3808b6b1ef6
CarlosVR <rricardez@live.com.ar>
2025-09-29 12:09:57 -0600

    SetExtraPropoerties

---
+++ src/App.School.v3.Domain/Entities/IdentityUserExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp.Data;
+using Volo.Abp.Identity;
+
+namespace App.School.v3.Entities
+{
+    public static class IdentityUserExtensions
+    {
+        private const string CustomerIdProperty = "SchoolId";
+        public static void SetUserSchoolId(this IdentityUser user, long customerId)
+        {
+            user.SetProperty(CustomerIdProperty, customerId);
+        }
+        public static int GetSchoolIdFromUSer(this IdentityUser user)
+        {
+            return user.GetProperty<int>(CustomerIdProperty);
+        }
+    }
+}
