eb02ceb56066093a57f961275511978536d551f5
Carlos Cicler <programador1972@gmail.com>
2025-10-05 15:30:57 -0600

    se limpian los using no usados en las entidades

--- src/App.School.v3.Domain/Entities/Document.cs
+++ src/App.School.v3.Domain/Entities/Document.cs
@@ -1,10 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Volo.Abp.Domain.Entities.Auditing;
+﻿using Volo.Abp.Domain.Entities.Auditing;
 
 namespace App.School.v3.Entities
 {
--- src/App.School.v3.Domain/Entities/Grade.cs
+++ src/App.School.v3.Domain/Entities/Grade.cs
@@ -1,9 +1,4 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace App.School.v3.Entities
